df6aa40e36d9546849bdac1b2a2df36d90cc0806
xiaolongren <52064463+Little-Dragon-God@users.noreply.github.com>
2021-06-10 09:00:29 +0800

    mybatis查询方式

--- mybatistest1/src/test/java/Test1.java
+++ mybatistest1/src/test/java/Test1.java
@@ -1,4 +1,5 @@
 import com.xls.pojo.Dept;
+import com.xls.pojo.Emp;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -10,6 +11,8 @@ import sun.security.util.Resources;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Test1 {
     private SqlSession sqlSession;
@@ -22,12 +25,28 @@ public class Test1 {
     }
 
     @Test
-    public void testFindAll(){
-
-        // 调用SQL语句
-        List<Dept> list = sqlSession.selectList("findAll");
-        for (Dept dept : list) {
-            System.out.println(dept);
+    public void testFindOne(){
+        //查询单个对象
+        List<Emp> list = sqlSession.selectList("findOne");
+        for (Emp emp : list) {
+            System.out.println(emp);
+        }
+    }
+    @Test
+    public void testFindList(){
+        //查询所有对象
+        List<Emp> list = sqlSession.selectList("Emp.findAll");
+        for (Emp emp : list) {
+            System.out.println(emp);
+        }
+    }
+    @Test
+    public void testFindMap(){
+        //查询所有对象,前一个emp映射id，后一个参数是emp表的字段名，得大写
+        Map<Integer,Emp> empMap = sqlSession.selectMap("findEmpMap","EMPNO");
+        Set<Integer> empnos = empMap.keySet();
+        for (Integer empno : empnos) {
+            System.out.println(empno+" :" +empMap.get(empno));
         }
 
     }
